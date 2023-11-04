b2096858118ba02b976a05eb53eea128a8025abe
Max Zheng <34936931+maxzh1999tw@users.noreply.github.com>
2023-11-04 13:47:21 +0800

    將 ClassListExporter 移至 Excely 專案，並於套件相依專案中進行匯出函式的擴充

--- Excely.EPPlus.LGPL/Exporters/ClassListExporter.cs
+++ Excely.EPPlus.LGPL/Exporters/ClassListExporter.cs
@@ -1,77 +1,24 @@
 ﻿using Excely.EPPlus.LGPL.TableWriters;
-using Excely.Shaders;
-using Excely.TableFactories;
 using OfficeOpenXml;
-using System.Reflection;
 
-namespace Excely.EPPlus.LGPL.Exporters
+namespace Excely.Exporters
 {
     /// <summary>
-    /// 提供以類別結構為欄位，將物件集合匯出的功能
+    /// 為 ClassListExporter 提供 Excel 相關的擴展
     /// </summary>
-    /// <typeparam name="T">欲匯出的類別</typeparam>
-    public class ClassListExporter<T> where T : class
+    public static class ClassListExporterExtension
     {
-        /// <summary>
-        /// 將 List 轉成 Table 的物件
-        /// </summary>
-        protected ClassListTableFactory<T> TableFactory { get; set; }
-
-        /// <summary>
-        /// 匯出資料完畢後依序執行的 IShader 集合
-        /// </summary>
-        public IEnumerable<IShader> Shaders { get; set; } = Enumerable.Empty<IShader>();
-
-        /// <summary>
-        /// 建構子
-        /// </summary>
-        /// <param name="propertyShowPolicy">
-        /// 決定 Property 是否應作為欄位匯出的執行邏輯，
-        /// 輸入參數為 PropertyInfo，輸出結果為「是否應作為欄位匯出」，
-        /// 若此屬性為 null，則全部欄位都匯出
-        /// </param>
-        /// <param name="propertyNamePolicy">
-        /// 決定 Property 作為欄位時的名稱
-        /// 輸入參數為 PropertyInfo，輸出結果為「欄位名稱」，
-        /// 若此屬性為 null，則欄位名稱為 PropertyInfo.Name
-        /// </param>
-        /// <param name="propertyOrderPolicy">
-        /// 決定 Property 作為欄位時的順序
-        /// 輸入參數為 PropertyInfo，輸出結果為「排序(由小到大)」，
-        /// 若此屬性為 null，則欄位依類別內預設排序
-        /// </param>
-        /// <param name="customValuePolicy">
-        /// 決定資料寫入欄位時的值
-        /// 輸入參數為 (當前匯出物件, PropertyInfo)，輸出結果為「欲寫入欄位的值」，
-        /// 若此屬性為 null，則值為該 Property 之 Value
-        /// </param>
-        public ClassListExporter(
-            Func<PropertyInfo, bool>? propertyShowPolicy = null,
-            Func<PropertyInfo, string?>? propertyNamePolicy = null,
-            Func<PropertyInfo, int>? propertyOrderPolicy = null,
-            Func<T, PropertyInfo, object?>? customValuePolicy = null
-            )
-        {
-            TableFactory = new ClassListTableFactory<T>()
-            {
-                PropertyShowPolicy = propertyShowPolicy,
-                PropertyNamePolicy = propertyNamePolicy,
-                PropertyOrderPolicy = propertyOrderPolicy,
-                CustomValuePolicy = customValuePolicy
-            };
-        }
-
         /// <summary>
         /// 將指定的物件集合匯出至特定的 Excel 工作表
         /// </summary>
         /// <param name="sourceData">來源資料</param>
         /// <param name="worksheet">指定的工作表</param>
-        public void ToWorksheet(IEnumerable<T> sourceData, ExcelWorksheet worksheet)
+        public static void ToWorksheet<T>(this ClassListExporter<T> exporter, IEnumerable<T> sourceData, ExcelWorksheet worksheet) where T : class
         {
-            var table = TableFactory.GetTable(sourceData);
+            var table = exporter.TableFactory.GetTable(sourceData);
             var tableWriter = new XlsxTableWriter(worksheet);
             tableWriter.Write(table);
-            foreach (var shaders in Shaders)
+            foreach (var shaders in exporter.Shaders)
             {
                 worksheet = shaders.Excute(worksheet);
             }
@@ -83,11 +30,11 @@ namespace Excely.EPPlus.LGPL.Exporters
         /// <param name="sourceData">來源資料</param>
         /// <param name="worksheetName">工作表名稱</param>
         /// <returns>全新的 Excel 實體</returns>
-        public ExcelPackage ToExcel(IEnumerable<T> sourceData, string worksheetName = "sheet1")
+        public static ExcelPackage ToExcel<T>(this ClassListExporter<T> exporter, IEnumerable<T> sourceData, string worksheetName = "sheet1") where T : class
         {
             var package = new ExcelPackage();
             var worksheet = package.Workbook.Worksheets.Add(worksheetName);
-            ToWorksheet(sourceData, worksheet);
+            exporter.ToWorksheet(sourceData, worksheet);
             return package;
         }
     }
---
+++ Excely/Exporters/ClassListExporter.cs
@@ -0,0 +1,62 @@
+﻿using Excely.Shaders;
+using Excely.TableFactories;
+using System.Reflection;
+
+namespace Excely.Exporters
+{
+    /// <summary>
+    /// 提供以類別結構為欄位，將物件集合匯出的功能
+    /// </summary>
+    /// <typeparam name="T">欲匯出的類別</typeparam>
+    public class ClassListExporter<T> where T : class
+    {
+        /// <summary>
+        /// 將 List 轉成 Table 的物件
+        /// </summary>
+        public ClassListTableFactory<T> TableFactory { get; init; }
+
+        /// <summary>
+        /// 匯出資料完畢後依序執行的 IShader 集合
+        /// </summary>
+        public IEnumerable<IShader> Shaders { get; set; } = Enumerable.Empty<IShader>();
+
+        /// <summary>
+        /// 建構子
+        /// </summary>
+        /// <param name="propertyShowPolicy">
+        /// 決定 Property 是否應作為欄位匯出的執行邏輯，
+        /// 輸入參數為 PropertyInfo，輸出結果為「是否應作為欄位匯出」，
+        /// 若此屬性為 null，則全部欄位都匯出
+        /// </param>
+        /// <param name="propertyNamePolicy">
+        /// 決定 Property 作為欄位時的名稱
+        /// 輸入參數為 PropertyInfo，輸出結果為「欄位名稱」，
+        /// 若此屬性為 null，則欄位名稱為 PropertyInfo.Name
+        /// </param>
+        /// <param name="propertyOrderPolicy">
+        /// 決定 Property 作為欄位時的順序
+        /// 輸入參數為 PropertyInfo，輸出結果為「排序(由小到大)」，
+        /// 若此屬性為 null，則欄位依類別內預設排序
+        /// </param>
+        /// <param name="customValuePolicy">
+        /// 決定資料寫入欄位時的值
+        /// 輸入參數為 (當前匯出物件, PropertyInfo)，輸出結果為「欲寫入欄位的值」，
+        /// 若此屬性為 null，則值為該 Property 之 Value
+        /// </param>
+        public ClassListExporter(
+            Func<PropertyInfo, bool>? propertyShowPolicy = null,
+            Func<PropertyInfo, string?>? propertyNamePolicy = null,
+            Func<PropertyInfo, int>? propertyOrderPolicy = null,
+            Func<T, PropertyInfo, object?>? customValuePolicy = null
+            )
+        {
+            TableFactory = new ClassListTableFactory<T>()
+            {
+                PropertyShowPolicy = propertyShowPolicy,
+                PropertyNamePolicy = propertyNamePolicy,
+                PropertyOrderPolicy = propertyOrderPolicy,
+                CustomValuePolicy = customValuePolicy
+            };
+        }
+    }
+}
