4564c39df90d2e01673eac77b08e6a7b091e3efd
Max Zheng <34936931+maxzh1999tw@users.noreply.github.com>
2023-11-04 10:44:10 +0800

    加入 ErrorMarkShader

--- Excely.Debug/Program.cs
+++ Excely.Debug/Program.cs
@@ -2,8 +2,10 @@
 using Excely.EPPlus.LGPL.Exporters;
 using Excely.EPPlus.LGPL.Shaders.Xlsx;
 using Excely.EPPlus.LGPL.TableFactories;
+using Excely.EPPlus.LGPL.TableWriters;
 using Excely.Shaders;
 using Excely.TableImporter;
+using OfficeOpenXml;
 
 namespace Excely.Debug
 {
@@ -34,10 +36,22 @@ namespace Excely.Debug
 
             var reader = new XlsxTableFactory();
             var table = reader.GetTable(excel.Workbook.Worksheets.First());
-            var list = new ClassListTableImporter<Student>()
+            var importResult = new ClassListTableImporter<Student>()
             {
                 StopWhenError = false
             }.Import(table);
+
+            if(importResult.CellErrors.Any())
+            {
+                var errorRows = table.Data.Where(x => importResult.CellErrors.Any(c => c.Key.Row == table.Data.IndexOf(x))).ToList();
+                errorRows.Insert(0, table.Data[0]);
+                var errTable = new ExcelyTable(errorRows);
+                using var errExcel = new ExcelPackage();
+                var sheet = errExcel.Workbook.Worksheets.Add("sheet1");
+                new XlsxTableWriter(sheet).Write(errTable);
+                new ErrorMarkShader(importResult.CellErrors.ToDictionary(x => x.Key, x => x.Value.Message), "Max").Excute(sheet);
+                errExcel.SaveAs(new FileInfo("err.xlsx"));
+            }
         }
     }
 }
---
+++ Excely.EPPlus.LGPL/Shaders/Xlsx/ErrorMarkShader.cs
@@ -0,0 +1,34 @@
+﻿using Excely.Shaders;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excely.EPPlus.LGPL.Shaders.Xlsx
+{
+    public class ErrorMarkShader : XlsxShaderBase
+    {
+        public Dictionary<CellLocation, string> CellErrors { get; set; }
+        public Color TextColor { get; set; } = Color.Red;
+        public string Author { get; set; }
+
+        public ErrorMarkShader(Dictionary<CellLocation, string> errorCells, string author)
+        {
+            CellErrors = errorCells;
+            Author = author;
+        }
+
+        protected override void ExcuteOnWorksheet(ExcelWorksheet worksheet)
+        {
+            foreach (var cellError in CellErrors)
+            {
+                var cell = worksheet.Cells[cellError.Key.Row+1, cellError.Key.Column+1];
+                cell.Style.Font.Color.SetColor(TextColor);
+                cell.AddComment(cellError.Value, Author);
+            }
+        }
+    }
+}
