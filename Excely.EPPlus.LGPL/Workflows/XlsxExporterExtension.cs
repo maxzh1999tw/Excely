d122316a989a407b82ec6d74446100480a23a70d
Max Zheng <34936931+maxzh1999tw@users.noreply.github.com>
2023-11-10 18:07:41 +0800

    調整匯出 Workflow 的參數，開放指定 StartCell

--- Excely.EPPlus.LGPL/Workflows/XlsxExporterExtension.cs
+++ Excely.EPPlus.LGPL/Workflows/XlsxExporterExtension.cs
@@ -10,10 +10,12 @@ namespace Excely.Workflows
         /// </summary>
         /// <param name="sourceData">來源資料</param>
         /// <param name="worksheet">指定的工作表</param>
-        public static void ToWorksheet<TInput>(this ExcelyExporter<TInput> exporter, TInput sourceData, ExcelWorksheet worksheet)
+        public static void ToWorksheet<TInput>(
+            this ExcelyExporter<TInput> exporter, TInput sourceData, 
+            ExcelWorksheet worksheet, CellLocation startCell = default)
         {
             var table = exporter.GetTable(sourceData);
-            var tableWriter = new XlsxTableConverter(worksheet);
+            var tableWriter = new XlsxTableConverter(worksheet, startCell);
             tableWriter.Convert(table);
             foreach (var shaders in exporter.Shaders)
             {
@@ -27,11 +29,14 @@ namespace Excely.Workflows
         /// <param name="sourceData">來源資料</param>
         /// <param name="worksheetName">工作表名稱</param>
         /// <returns>全新的 Excel 實體</returns>
-        public static ExcelPackage ToExcel<TInput>(this ExcelyExporter<TInput> exporter, TInput sourceData, string worksheetName = "sheet1")
+        public static ExcelPackage ToExcel<TInput>(
+            this ExcelyExporter<TInput> exporter, TInput sourceData, 
+            string worksheetName = "sheet1",
+            CellLocation startCell = default)
         {
             var package = new ExcelPackage();
             var worksheet = package.Workbook.Worksheets.Add(worksheetName);
-            exporter.ToWorksheet(sourceData, worksheet);
+            exporter.ToWorksheet(sourceData, worksheet, startCell);
             return package;
         }
     }
