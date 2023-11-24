21cb0017ccccf8baf70e916f81bbccfbe91dd2a4
Max Zheng <34936931+maxzh1999tw@users.noreply.github.com>
2023-11-24 19:01:14 +0800

    新增單元測試

---
+++ Excely.UnitTests/CellLocationTests.cs
@@ -0,0 +1,88 @@
+﻿namespace Excely.UnitTests
+{
+    [TestClass]
+    public class CellLocationTests
+    {
+        [TestMethod]
+        public void Constructor_SetsRowAndColumn()
+        {
+            // Arrange
+            int testRow = 5;
+            int testColumn = 10;
+
+            // Act
+            CellLocation cellLocation = new CellLocation(testRow, testColumn);
+
+            // Assert
+            Assert.AreEqual(testRow, cellLocation.Row);
+            Assert.AreEqual(testColumn, cellLocation.Column);
+        }
+
+        [TestMethod]
+        public void Equals_ReturnsTrueForEqualObjects()
+        {
+            // Arrange
+            CellLocation a = new CellLocation(5, 10);
+            CellLocation b = new CellLocation(5, 10);
+
+            // Act & Assert
+            Assert.IsTrue(a.Equals(b));
+        }
+
+        [TestMethod]
+        public void Equals_ReturnsFalseForDifferentObjects()
+        {
+            // Arrange
+            CellLocation a = new CellLocation(5, 10);
+            CellLocation b = new CellLocation(6, 11);
+
+            // Act & Assert
+            Assert.IsFalse(a.Equals(b));
+        }
+
+        [TestMethod]
+        public void EqualityOperator_ReturnsTrueForEqualObjects()
+        {
+            // Arrange
+            CellLocation a = new CellLocation(5, 10);
+            CellLocation b = new CellLocation(5, 10);
+
+            // Act & Assert
+            Assert.IsTrue(a == b);
+        }
+
+        [TestMethod]
+        public void InequalityOperator_ReturnsFalseForEqualObjects()
+        {
+            // Arrange
+            CellLocation a = new CellLocation(5, 10);
+            CellLocation b = new CellLocation(5, 10);
+
+            // Act & Assert
+            Assert.IsFalse(a != b);
+        }
+
+        [TestMethod]
+        public void GetHashCode_ReturnsDifferentValuesForDifferentObjects()
+        {
+            // Arrange
+            CellLocation a = new CellLocation(5, 10);
+            CellLocation b = new CellLocation(6, 11);
+
+            // Act & Assert
+            Assert.AreNotEqual(a.GetHashCode(), b.GetHashCode());
+        }
+
+        [TestMethod]
+        public void Equals_ReturnsFalseWhenComparedWithNonCellLocationObject()
+        {
+            // Arrange
+            CellLocation cellLocation = new CellLocation(5, 10);
+            object nonCellLocationObject = new { Row = 5, Column = 10 };
+
+            // Act & Assert
+            Assert.IsFalse(cellLocation.Equals(nonCellLocationObject));
+        }
+
+    }
+}
---
+++ Excely.UnitTests/ClassListTableConverterTests.cs
@@ -0,0 +1,181 @@
+﻿using Excely.TableConverters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excely.UnitTests
+{
+    [TestClass]
+    public class ClassListTableConverterTests
+    {
+        public class SampleClass
+        {
+            public int Id { get; set; }
+            public string? Name { get; set; }
+            public double? Value { get; set; }
+        }
+
+        // 基本轉換測試
+        [TestMethod]
+        public void Convert_ShouldReturnCorrectListOfSampleClass()
+        {
+            // Arrange
+            var table = new ExcelyTable(new List<IList<object?>>
+            {
+                new List<object?> { "Id", "Name", "Value" },
+                new List<object?> { 1, "Item 1", 10.0 },
+                new List<object?> { 2, "Item 2", 20.0 }
+            });
+            var converter = new ClassListTableConverter<SampleClass>();
+
+            // Act
+            var result = converter.Convert(table).ToList();
+
+            // Assert
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual("Item 1", result[0].Name);
+            Assert.AreEqual(1, result[0].Id);
+            Assert.AreEqual(10.0, result[0].Value);
+        }
+
+        // 空值轉換測試
+        [TestMethod]
+        public void Convert_ShouldHandleNullValues()
+        {
+            // Arrange
+            var table = new ExcelyTable(new List<IList<object?>>
+            {
+                new List<object?> { "Id", "Name", "Value" },
+                new List<object?> { null, null, null }
+            });
+            var converter = new ClassListTableConverter<SampleClass>();
+
+            // Act
+            var result = converter.Convert(table).ToList();
+
+            // Assert
+            Assert.AreEqual(1, result.Count);
+            Assert.IsNull(result[0].Name);
+            Assert.AreEqual(0, result[0].Id);
+            Assert.IsNull(result[0].Value);
+        }
+
+        // 錯誤處理測試
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Convert_ShouldThrowExceptionOnError()
+        {
+            // Arrange
+            var table = new ExcelyTable(new List<IList<object?>>
+            {
+                new List<object?> { "Id", "Name", "Value" },
+                new List<object?> { "Invalid", "Item", 10.0 }
+            });
+            var converter = new ClassListTableConverter<SampleClass>();
+
+            // Act
+            converter.Convert(table).ToList();
+        }
+
+        [TestMethod]
+        public void Convert_WithCustomPropertyNamePolicy_ShouldMatchCorrectly()
+        {
+            var options = new ClassListTableConverterOptions<SampleClass>
+            {
+                PropertyNamePolicy = prop => prop.Name.ToUpper()
+            };
+            var table = new ExcelyTable(new List<IList<object?>>
+            {
+                new List<object?> { "ID", "NAME", "VALUE" },
+                new List<object?> { 1, "Item 1", 10.0 }
+            });
+            var converter = new ClassListTableConverter<SampleClass>(options);
+
+            var result = converter.Convert(table).FirstOrDefault();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Item 1", result.Name);
+        }
+
+        [TestMethod]
+        public void Convert_WithCustomPropertyIndexPolicy_ShouldMatchCorrectly()
+        {
+            var options = new ClassListTableConverterOptions<SampleClass>
+            {
+                HasSchema = false,
+                PropertyIndexPolicy = (props, prop) => prop.Name == "Name" ? 1 : Array.IndexOf(props, prop)
+            };
+            var table = new ExcelyTable(new List<IList<object?>>
+            {
+                new List<object?> { 1, "Item 1", 10.0 }
+            });
+            var converter = new ClassListTableConverter<SampleClass>(options);
+
+            var result = converter.Convert(table).FirstOrDefault();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Item 1", result.Name);
+        }
+
+        [TestMethod]
+        public void Convert_WithCustomValuePolicy_ShouldTransformValues()
+        {
+            var options = new ClassListTableConverterOptions<SampleClass>
+            {
+                CustomValuePolicy = (prop, obj) => prop.Name == "Name" ? obj?.ToString()?.ToUpper() : obj
+            };
+            var table = new ExcelyTable(new List<IList<object?>>
+            {
+                new List<object?> { "Id", "Name", "Value" },
+                new List<object?> { 1, "item", 10.0 }
+            });
+            var converter = new ClassListTableConverter<SampleClass>(options);
+
+            var result = converter.Convert(table).FirstOrDefault();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("ITEM", result.Name);
+        }
+
+        [TestMethod]
+        public void Convert_WithCustomErrorHandlingPolicy_ShouldHandleErrors()
+        {
+            var options = new ClassListTableConverterOptions<SampleClass>
+            {
+                StopWhenError = false,
+            };
+            var table = new ExcelyTable(new List<IList<object?>>
+            {
+                new List<object?> { "Id", "Name", "Value" },
+                new List<object?> { "Invalid", "Item", 10.0 }
+            });
+            var converter = new ClassListTableConverter<SampleClass>(options);
+
+            var result = converter.Convert(table).ToList();
+
+            Assert.AreEqual(0, result.Count);  // 預期忽略錯誤的行不會被添加
+        }
+
+        [TestMethod]
+        public void Convert_WithHasSchemaFalse_ShouldIgnoreHeader()
+        {
+            var options = new ClassListTableConverterOptions<SampleClass>
+            {
+                HasSchema = false
+            };
+            var table = new ExcelyTable(new List<IList<object?>>
+            {
+                new List<object?> { 1, "Item 1", 10.0 },
+                new List<object?> { 1, "Item 2", 10.0 },
+            });
+            var converter = new ClassListTableConverter<SampleClass>(options);
+
+            var result = converter.Convert(table).ToList();
+
+            Assert.AreEqual(2, result.Count);  // 包括標題行和數據行
+        }
+
+    }
+}
