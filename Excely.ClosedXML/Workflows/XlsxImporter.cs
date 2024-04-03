0e5c9ebf073b902b516cda6c82b68513476b6035
max <max@shin-her.com.tw>
2024-04-03 16:56:07 +0800

    Workflow 新增從檔案路徑直接讀取的函式

--- Excely.ClosedXML/Workflows/XlsxImporter.cs
+++ Excely.ClosedXML/Workflows/XlsxImporter.cs
@@ -1,29 +1,39 @@
 ﻿using ClosedXML.Excel;
 using Excely.ClosedXML.TableFactories;
+using Excely.TableConverters;
 
 namespace Excely.Workflows
 {
-    /// <summary>
-    /// 提供快速建立從 Excel 到指定資料結構之 Importer 的方法。
-    /// </summary>
-    public class XlsxImporter : ExcelyImporterBase<IXLWorksheet>
-    {
-        protected XlsxTableFactory XlsxTableFactory { get; set; } = new XlsxTableFactory();
+	/// <summary>
+	/// 提供快速建立從 Excel 到指定資料結構之 Importer 的方法。
+	/// </summary>
+	public class XlsxImporter : ExcelyImporterBase<IXLWorksheet>
+	{
+		protected XlsxTableFactory XlsxTableFactory { get; set; } = new XlsxTableFactory();
 
-        #region === 建構子 ==
-        public XlsxImporter() { }
+		#region === 建構子 ==
 
-        public XlsxImporter(CellLocation? startCell, CellLocation? endCell)
-        {
-            XlsxTableFactory = new XlsxTableFactory();
-            if (startCell != null)
-            {
-                XlsxTableFactory.StartCell = startCell.Value;
-            }
-            XlsxTableFactory.EndCell = endCell;
-        }
-        #endregion
+		public XlsxImporter()
+		{ }
 
-        protected override ExcelyTable GetTable(IXLWorksheet input) => XlsxTableFactory.GetTable(input);
-    }
+		public XlsxImporter(CellLocation? startCell, CellLocation? endCell)
+		{
+			XlsxTableFactory = new XlsxTableFactory();
+			if (startCell != null)
+			{
+				XlsxTableFactory.StartCell = startCell.Value;
+			}
+			XlsxTableFactory.EndCell = endCell;
+		}
+
+		#endregion === 建構子 ==
+
+		protected override ExcelyTable GetTable(IXLWorksheet input) => XlsxTableFactory.GetTable(input);
+
+		protected override IXLWorksheet GetDataSource(string filePath)
+		{
+			var workbook = new XLWorkbook(filePath);
+			return workbook.Worksheet(0);
+		}
+	}
 }
--- Excely/Workflows/ExcelyImporterBase.cs
+++ Excely/Workflows/ExcelyImporterBase.cs
@@ -2,45 +2,63 @@
 
 namespace Excely.Workflows
 {
-    /// <summary>
-    /// 提供從資料輸入到轉換為資料結構的完整工作流程之基底類別。
-    /// </summary>
-    /// <typeparam name="TInput">資料的輸入型別</typeparam>
-    public abstract class ExcelyImporterBase<TInput>
-    {
-        protected abstract ExcelyTable GetTable(TInput input);
+	/// <summary>
+	/// 提供從資料輸入到轉換為資料結構的完整工作流程之基底類別。
+	/// </summary>
+	/// <typeparam name="TInput">資料的輸入型別</typeparam>
+	public abstract class ExcelyImporterBase<TInput>
+	{
+		protected abstract ExcelyTable GetTable(TInput input);
 
-        /// <summary>
-        /// 將資料匯入為物件列表。
-        /// </summary>
-        /// <typeparam name="TClass">目標類別</typeparam>
-        /// <param name="dataSource">資料來源</param>
-        /// <param name="options">匯入邏輯</param>
-        /// <returns>匯入結果</returns>
-        public IEnumerable<TClass> ToClassList<TClass>(
-            TInput dataSource,
-            ClassListTableConverterOptions<TClass>? options = null)
-            where TClass : class, new()
-        {
-            var table = GetTable(dataSource);
-            var converter = options == null ? new ClassListTableConverter<TClass>() : new ClassListTableConverter<TClass>(options);
-            return converter.ConvertFrom(table);
-        }
+		/// <summary>
+		/// 將資料匯入為物件列表。
+		/// </summary>
+		/// <typeparam name="TClass">目標類別</typeparam>
+		/// <param name="dataSource">資料來源</param>
+		/// <param name="options">匯入邏輯</param>
+		/// <returns>匯入結果</returns>
+		public IEnumerable<TClass> ToClassList<TClass>(
+			TInput dataSource,
+			ClassListTableConverterOptions<TClass>? options = null)
+			where TClass : class, new()
+		{
+			var table = GetTable(dataSource);
+			var converter = options == null ? new ClassListTableConverter<TClass>() : new ClassListTableConverter<TClass>(options);
+			return converter.ConvertFrom(table);
+		}
 
-        /// <summary>
-        /// 將資料匯入為字典列表。
-        /// </summary>=
-        /// <param name="dataSource">資料來源</param>
-        /// <param name="options">匯入邏輯</param>
-        /// <returns>匯入結果</returns>
-        public IEnumerable<Dictionary<string, object?>> ToDictionaryList(
-            TInput dataSource,
-            DictionaryListTableConverterOptions? options = null)
-        {
-            var table = GetTable(dataSource);
-            var converter = options == null ? new DictionaryListTableConverter() : new DictionaryListTableConverter(options);
-            return converter.ConvertFrom(table);
-        }
-    }
+		/// <summary>
+		/// 將資料匯入為字典列表。
+		/// </summary>=
+		/// <param name="dataSource">資料來源</param>
+		/// <param name="options">匯入邏輯</param>
+		/// <returns>匯入結果</returns>
+		public IEnumerable<Dictionary<string, object?>> ToDictionaryList(
+			TInput dataSource,
+			DictionaryListTableConverterOptions? options = null)
+		{
+			var table = GetTable(dataSource);
+			var converter = options == null ? new DictionaryListTableConverter() : new DictionaryListTableConverter(options);
+			return converter.ConvertFrom(table);
+		}
+
+		protected abstract TInput GetDataSource(string filePath);
+
+		public IEnumerable<TClass> ToClassList<TClass>(
+			string filePath,
+			ClassListTableConverterOptions<TClass>? options = null)
+			where TClass : class, new()
+		{
+			var dataSource = GetDataSource(filePath);
+			return ToClassList(dataSource, options);
+		}
+
+		public IEnumerable<Dictionary<string, object?>> ToDictionaryList(
+			string filePath,
+			DictionaryListTableConverterOptions? options = null)
+		{
+			var dataSource = GetDataSource(filePath);
+			return ToDictionaryList(dataSource, options);
+		}
+	}
 }
-
