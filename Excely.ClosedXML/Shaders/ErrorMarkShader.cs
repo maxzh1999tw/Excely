4f398e20c24ac4cef5889da58d0a5f3872b61288
max <max@shin-her.com.tw>
2024-10-15 15:59:58 +0800

    ErrorMarkShader 現在會預設自動生成必要 property

--- Excely.ClosedXML/Shaders/ErrorMarkShader.cs
+++ Excely.ClosedXML/Shaders/ErrorMarkShader.cs
@@ -1,36 +1,39 @@
 ﻿using ClosedXML.Excel;
+using System.Formats.Asn1;
+using System.Reflection;
 
 namespace Excely.ClosedXML.Shaders
 {
-    /// <summary>
-    /// 將出錯的表格醒目標示，並標註錯誤原因。
-    /// </summary>
-    public class ErrorMarkShader : XlsxShaderBase
-    {
-        /// <summary>
-        /// 匯入錯誤資料。
-        /// </summary>
-        public Dictionary<CellLocation, string> CellErrors { get; set; }
+	/// <summary>
+	/// 將出錯的表格醒目標示，並標註錯誤原因。
+	/// </summary>
+	public class ErrorMarkShader : XlsxShaderBase
+	{
+		/// <summary>
+		/// 匯入錯誤資料。
+		/// </summary>
+		public Dictionary<CellLocation, string> CellErrors { get; set; }
 
-        /// <summary>
-        /// 醒目標示文字顏色。
-        /// </summary>
-        public XLColor TextColor { get; set; } = XLColor.Red;
+		/// <summary>
+		/// 醒目標示文字顏色。
+		/// </summary>
+		public XLColor TextColor { get; set; } = XLColor.Red;
 
-        /// <param name="errorCells">匯入錯誤資料</param>
-        public ErrorMarkShader(Dictionary<CellLocation, string> errorCells)
-        {
-            CellErrors = errorCells;
-        }
+		/// <param name="errorCells">匯入錯誤資料</param>
+		public ErrorMarkShader(Dictionary<CellLocation, string>? errorCells = null)
+		{
+			CellErrors = errorCells ?? new();
+		}
 
-        protected override void ExcuteOnWorksheet(IXLWorksheet worksheet)
-        {
-            foreach (var cellError in CellErrors)
-            {
-                var cell = worksheet.Cell(cellError.Key.Row + 1, cellError.Key.Column + 1);
-                cell.Style.Font.SetFontColor(TextColor);
-                cell.GetComment().AddText(cellError.Value);
-            }
-        }
-    }
+		protected override void ExcuteOnWorksheet(IXLWorksheet worksheet)
+		{
+			foreach (var cellError in CellErrors)
+			{
+				var cell = worksheet.Cell(cellError.Key.Row + 1, cellError.Key.Column + 1);
+				cell.Style.Font.SetFontColor(TextColor);
+				cell.GetComment().Delete();
+				cell.GetComment().AddText(cellError.Value);
+			}
+		}
+	}
 }
--- Excely.EPPlus.LGPL/Shaders/ErrorMarkShader.cs
+++ Excely.EPPlus.LGPL/Shaders/ErrorMarkShader.cs
@@ -3,47 +3,47 @@ using System.Drawing;
 
 namespace Excely.EPPlus.LGPL.Shaders
 {
-    /// <summary>
-    /// 將出錯的表格醒目標示，並標註錯誤原因。
-    /// </summary>
-    public class ErrorMarkShader : XlsxShaderBase
-    {
-        /// <summary>
-        /// 匯入錯誤資料。
-        /// </summary>
-        public Dictionary<CellLocation, string> CellErrors { get; set; }
+	/// <summary>
+	/// 將出錯的表格醒目標示，並標註錯誤原因。
+	/// </summary>
+	public class ErrorMarkShader : XlsxShaderBase
+	{
+		/// <summary>
+		/// 匯入錯誤資料。
+		/// </summary>
+		public Dictionary<CellLocation, string> CellErrors { get; set; }
 
-        /// <summary>
-        /// 醒目標示文字顏色。
-        /// </summary>
-        public Color TextColor { get; set; } = Color.Red;
+		/// <summary>
+		/// 醒目標示文字顏色。
+		/// </summary>
+		public Color TextColor { get; set; } = Color.Red;
 
-        /// <summary>
-        /// Comment 作者(不得為空)。
-        /// </summary>
-        public string Author { get; set; }
+		/// <summary>
+		/// Comment 作者(不得為空)。
+		/// </summary>
+		public string Author { get; set; }
 
-        /// <param name="errorCells">匯入錯誤資料</param>
-        /// <param name="author">Comment 作者(不得為空)</param>
-        public ErrorMarkShader(Dictionary<CellLocation, string> errorCells, string author)
-        {
-            if (string.IsNullOrEmpty(author))
-            {
-                throw new ArgumentException($"{nameof(author)} 參數不得為空。");
-            }
+		/// <param name="errorCells">匯入錯誤資料</param>
+		/// <param name="author">Comment 作者(不得為空)</param>
+		public ErrorMarkShader(string author, Dictionary<CellLocation, string>? errorCells = null)
+		{
+			if (string.IsNullOrEmpty(author))
+			{
+				throw new ArgumentException($"{nameof(author)} 參數不得為空。");
+			}
 
-            CellErrors = errorCells;
-            Author = author;
-        }
+			CellErrors = errorCells ?? new();
+			Author = author;
+		}
 
-        protected override void ExcuteOnWorksheet(ExcelWorksheet worksheet)
-        {
-            foreach (var cellError in CellErrors)
-            {
-                var cell = worksheet.Cells[cellError.Key.Row + 1, cellError.Key.Column + 1];
-                cell.Style.Font.Color.SetColor(TextColor);
-                cell.AddComment(cellError.Value, Author);
-            }
-        }
-    }
+		protected override void ExcuteOnWorksheet(ExcelWorksheet worksheet)
+		{
+			foreach (var cellError in CellErrors)
+			{
+				var cell = worksheet.Cells[cellError.Key.Row + 1, cellError.Key.Column + 1];
+				cell.Style.Font.Color.SetColor(TextColor);
+				cell.AddComment(cellError.Value, Author);
+			}
+		}
+	}
 }
